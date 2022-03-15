a831510e7a571bc7ca1f2d776e491148692db371
Selahattin Kara <Selahattin Kara@DESKTOP-JNQH4OR>
2022-03-15 15:18:21 +0300

    Add project files.

---
+++ Urunler/Default.aspx
@@ -0,0 +1,54 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Urunler.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body style="background-color:darkgoldenrod">
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Ürün Kodu"></asp:Label>
+            <asp:TextBox ID="tbxUrunKodu" runat="server" style="margin-left: 43px; margin-bottom: 0px"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Ürün Adı"></asp:Label>
+            <asp:TextBox ID="tbxUrunAdi" runat="server" style="margin-left: 56px; margin-bottom: 0px"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="Label3" runat="server" Text="Stok Miktarı"></asp:Label>
+            <asp:TextBox ID="tbxStokMiktari" runat="server" style="margin-left: 34px; margin-bottom: 0px" Width="121px"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="Label4" runat="server" Text="Birim Fiyat"></asp:Label>
+            <asp:TextBox ID="tbxBirimFiyati" runat="server" style="margin-left: 44px; margin-bottom: 0px" Width="123px"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="Label5" runat="server" Text="Ürün Açıklama"></asp:Label>
+        </div>
+        <p>
+            <asp:TextBox ID="tbxUrunAciklama" runat="server" Height="56px" style="margin-left: 3px; margin-top: 0px; margin-bottom: 0px" Width="223px"></asp:TextBox>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Label ID="Label6" runat="server" Text="Sonuç:"></asp:Label>
+&nbsp;
+            <asp:Label ID="lblSonuc" runat="server"></asp:Label>
+        </p>
+        <p>
+            <asp:Button ID="btnEkle" runat="server" Height="44px" OnClick="btnEkle_Click" Text="Ürün Ekle" Width="79px" />
+&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btnSil" runat="server" Height="44px" Text="Ürün Sil" Width="79px" OnClick="btnSil_Click" />
+&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btnGuncelle" runat="server" Height="44px" Text="Ürün Güncelle" Width="79px" OnClick="btnGuncelle_Click" />
+&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btnListele" runat="server" Height="44px" OnClick="btnListele_Click" Text="Ürünleri Listele" Width="79px" />
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:GridView ID="GridView1" BackColor="White" runat="server" Height="165px" Width="996px">
+            </asp:GridView>
+        </p>
+    </form>
+</body>
+</html>
---
+++ Urunler/Default.aspx.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Urunler
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        Urunler_VTEntities baglanti = new Urunler_VTEntities();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnEkle_Click(object sender, EventArgs e)
+        {
+            Urunler urunEkle = new Urunler();
+            urunEkle.UrunKodu = int.Parse(tbxUrunKodu.Text);
+            urunEkle.UrunAdi = tbxUrunAdi.Text;
+            urunEkle.BirimFiyati = int.Parse(tbxBirimFiyati.Text);
+            urunEkle.StokMiktari = int.Parse(tbxStokMiktari.Text);
+            urunEkle.UrunAciklamasi = tbxUrunAciklama.Text;
+
+            try
+            {
+                baglanti.Urunler.Add(urunEkle);
+                baglanti.SaveChanges();
+                lblSonuc.Text = "Kayıt Başarıyla Gerçekleşti.";
+            }
+            catch (Exception)
+            {
+                lblSonuc.Text = "Kayıt Eklenemedi";
+
+            }
+        }
+
+        protected void btnListele_Click(object sender, EventArgs e)
+        {
+            baglanti.Urunler.Load();
+            GridView1.DataSource = baglanti.Urunler.Local;
+            GridView1.DataBind();
+        }
+
+        protected void btnSil_Click(object sender, EventArgs e)
+        {
+            int urunKodu = int.Parse(tbxUrunKodu.Text);
+            var sorgu = (from kayit in baglanti.Urunler
+                         where kayit.UrunKodu == urunKodu
+                         select kayit).ToList();
+            if (sorgu.Count == 1)
+            {
+                baglanti.Urunler.Remove(sorgu[0]);
+                baglanti.SaveChanges();
+                lblSonuc.Text = "Kayıt Başarı İle Silindi";
+            }
+            else
+            {
+                lblSonuc.Text = "Kayıt Bulunamadı Yada Silme de Hata Çıktı";
+            }
+        }
+
+        protected void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            int urunKodu = Convert.ToInt32(tbxUrunKodu.Text);
+
+            var sorgu = (from kayit in baglanti.Urunler
+                         where kayit.UrunKodu == urunKodu
+                         select kayit).ToList();
+            if (sorgu.Count==1)
+            {
+                sorgu[0].UrunAdi = tbxUrunAdi.Text;
+                sorgu[0].BirimFiyati = int.Parse(tbxBirimFiyati.Text);
+                sorgu[0].StokMiktari = int.Parse(tbxStokMiktari.Text);
+                sorgu[0].UrunAciklamasi = tbxUrunAciklama.Text;
+                baglanti.SaveChanges();
+                lblSonuc.Text = "Kayıt Başarı İle Güncellendi";
+
+            }
+            else
+            {
+                lblSonuc.Text = "Kayıt Bulunamadı Yada Güncellemede Hata Çıktı";
+            }
+        }
+    }
+}
